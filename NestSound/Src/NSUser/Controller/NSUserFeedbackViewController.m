f1812e8654f3699f9290903acd903695ba4e4278
张轩赫 <zhxhlove@sina.com>
2016-06-24 14:26:28 +0800

    1

--- NestSound/Src/NSUser/Controller/NSUserFeedbackViewController.m
+++ NestSound/Src/NSUser/Controller/NSUserFeedbackViewController.m
@@ -38,7 +38,6 @@ UITextFieldDelegate
     [super viewDidLoad];
     
    
-
 }
 
 -(void)feedBackWithContent:(NSString *)content_ andNumber:(NSString *)number
@@ -46,11 +45,7 @@ UITextFieldDelegate
     self.requestType = NO;
     
     self.requestParams= @{@"userid":JUserID,@"text":[NSString stringWithFormat:@"%@",content_],@"phone":[NSString stringWithFormat:@"%@",number],@"token":LoginToken};
-<<<<<<< HEAD
 
-=======
-    
->>>>>>> a5ba1170361d7268127e650d9a8dab28d96f9cec
     if ([Type isEqualToString:@"feedBack"]) {
         self.requestURL = feedBackURL;
         
