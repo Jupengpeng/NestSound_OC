57549d8991b7b75e5e8093d18a1cea5820671192
谢豪杰 <3079426109@qq.com>
2016-07-08 17:23:19 +0800

    push lyricVC

--- NestSound/Src/NSUserPage/Controller/NSUserPageViewController.m
+++ NestSound/Src/NSUserPage/Controller/NSUserPageViewController.m
@@ -504,7 +504,12 @@ UITableViewDataSource>
         }
     }else if (type == 2){
         NSLyricViewController * lyricVC =[[NSLyricViewController alloc] initWithItemId:myMusic.itemId];
-        lyricVC.who = self.who;
+        if (self.who == Myself ) {
+            lyricVC.who = My;
+        }else{
+            lyricVC.who = His;
+        }
+        
         [self.navigationController pushViewController:lyricVC animated:YES];
     }else if (type == 3){
     
