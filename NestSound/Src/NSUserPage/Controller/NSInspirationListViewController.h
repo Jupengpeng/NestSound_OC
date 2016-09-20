8fa383a623393f37f322fc0c4872ce28246caa45
Lonfey <lonfey6@163.com>
2016-09-20 18:20:04 +0800

    个人中心界面修改 修复不能评论bug 推送消息处理

---
+++ NestSound/Src/NSUserPage/Controller/NSInspirationListViewController.h
@@ -0,0 +1,13 @@
+//
+//  NSInspirationListViewController.h
+//  NestSound
+//
+//  Created by yinchao on 16/9/20.
+//  Copyright © 2016年 yinchao. All rights reserved.
+//
+
+#import "NSBaseViewController.h"
+
+@interface NSInspirationListViewController : NSBaseViewController
+
+@end
