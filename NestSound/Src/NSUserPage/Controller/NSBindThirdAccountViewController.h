b6cc9fc8a6d2c720e37f14419a2db5b8ec30f0e6
Lonfey <lonfey6@163.com>
2016-09-22 18:45:35 +0800

    绑定第三方账号 完善歌曲 歌词 灵感纪录 收藏数据

---
+++ NestSound/Src/NSUserPage/Controller/NSBindThirdAccountViewController.h
@@ -0,0 +1,13 @@
+//
+//  NSBindThirdAccountViewController.h
+//  NestSound
+//
+//  Created by yinchao on 16/9/22.
+//  Copyright © 2016年 yinchao. All rights reserved.
+//
+
+#import "NSBaseViewController.h"
+
+@interface NSBindThirdAccountViewController : NSBaseViewController
+
+@end
