10ae928124909fe0efa18ec77ca28b7be6a30b1b
liuxiangwei <1670452415@qq.com>
2016-10-17 18:06:01 +0800

    提交者：鞠鹏 修改部分：保全协议添加

--- NestSound/Src/NSUser/View/NSPreserveWorkInfoCell.m
+++ NestSound/Src/NSUser/View/NSPreserveWorkInfoCell.m
@@ -173,9 +173,10 @@
     self.lyricsTitle.text = GetLyricsName(productModel.lyricAuthor);
     self.musicTitle.text = GetMusicName(productModel.songAuthor);
     self.accompanyTitle.text = GetAccompanyName(productModel.accompaniment);
-    self.createTime.text = GetcreaTime(productModel.createTime);
+    self.createTime.text = GetcreaTime([date datetoLongLongStringWithDate:productModel.createTime]);
     self.preserveDate.text = GetPreserveDate([date datetoLongLongStringWithDate:productModel.preserveTime]);
     self.preserveCode.text = GetPreserveCode(productModel.preserveNo);
+    
 }
 
 - (void)layoutSubviews{
