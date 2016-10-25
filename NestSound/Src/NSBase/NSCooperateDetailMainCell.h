930479b48a40ebb09eb7a789470a3df70a2a9433
liuxiangwei <1670452415@qq.com>
2016-10-25 19:06:46 +0800

    提交者：鞠鹏 修改部分：合作详情开始

---
+++ NestSound/Src/NSBase/NSCooperateDetailMainCell.h
@@ -0,0 +1,13 @@
+//
+//  NSCooperateDetailMainCell.h
+//  NestSound
+//
+//  Created by yintao on 2016/10/25.
+//  Copyright © 2016年 yinchao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NSCooperateDetailMainCell : UITableViewCell
+
+@end
