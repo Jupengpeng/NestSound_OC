a163d0fd8cb1d4d5a036d0828794cfe21b31cbfa
Lonfey <lonfey6@163.com>
2016-10-25 19:06:03 +0800

    创建合作类目

---
+++ NestSound/Src/Cooperation/NSCooperationViewController.h
@@ -0,0 +1,13 @@
+//
+//  NSCooperationViewController.h
+//  NestSound
+//
+//  Created by yinchao on 16/10/24.
+//  Copyright © 2016年 yinchao. All rights reserved.
+//
+
+#import "NSBaseViewController.h"
+
+@interface NSCooperationViewController : NSBaseViewController
+
+@end
---
+++ NestSound/Src/Cooperation/NSMainCooperationViewController.h
@@ -0,0 +1,13 @@
+//
+//  NSMainCooperationViewController.h
+//  NestSound
+//
+//  Created by yinchao on 16/10/25.
+//  Copyright © 2016年 yinchao. All rights reserved.
+//
+
+#import "NSBaseViewController.h"
+
+@interface NSMainCooperationViewController : NSBaseViewController
+
+@end
