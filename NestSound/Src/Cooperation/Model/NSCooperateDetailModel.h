394bbaad5d0d1ab976052bb62ca9dba033aa9f60
liuxiangwei <1670452415@qq.com>
2016-10-26 14:48:35 +0800

    提交者：鞠鹏 修改部分：请求超时修改，合作详情

---
+++ NestSound/Src/Cooperation/Model/NSCooperateDetailModel.h
@@ -0,0 +1,13 @@
+//
+//  NSCooperateDetailModel.h
+//  NestSound
+//
+//  Created by yintao on 2016/10/26.
+//  Copyright © 2016年 yinchao. All rights reserved.
+//
+
+#import "NSBaseModel.h"
+
+@interface NSCooperateDetailModel : NSBaseModel
+
+@end
